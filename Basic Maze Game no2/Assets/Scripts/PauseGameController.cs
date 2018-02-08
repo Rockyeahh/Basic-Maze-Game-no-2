15977277068611aa1e9af40da6ab75849b41ed5a
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-02-08 19:18:32 +0000

    Basic Maze Game no2 - Alpha built
    Game now ready for polish and maybe addition of a soundtrack, magic beans, rolling ball and so on.

---
+++ Basic Maze Game no2/Assets/Scripts/PauseGameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Pause()
+    {
+        // speed = 0;
+        // canvas ticked
+        // enabled mouse pointer
+        // 
+    }
+
+    public void UnPause()
+    {
+        // disable mouse pointer
+        // speed set to the original number
+        // canvas un-ticked
+    }
+
+}
---
+++ Basic Maze Game no2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public GameObject canvas;
+
+
+	// Use this for initialization
+	void Start () {
+        //Canvas canvas = canvas.GetComponent<Canvas>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        speed = 0;
+        Cursor.visible = true; // enabled mouse pointer
+                               // canvas ticked = 
+        canvas.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        speed = 10; // I will need to find a way to set it to whatever the inspector currently is.
+        Cursor.visible = false; // disable mouse pointer
+                                // canvas un-ticked = 
+        canvas.SetActive(false);
+    }
+
+}
